eff1fce3afb0cf1b5135d1b9708ad9d938faea5d
Vladimir <vladbeer@bk.ru>
2020-05-13 00:00:55 +0300

    first game

---
+++ index.js
@@ -0,0 +1,91 @@
+var $start = document.querySelector('#start');
+var $game = document.querySelector('#game');
+var $time = document.querySelector('#time');
+var $result = document.querySelector('#result');
+var $timeHeader = document.querySelector('#time-header');
+var $resultHeader = document.querySelector('#result-header');
+var $gameTime = document.querySelector('#game-time');
+
+var score = 0;
+var isGameStarted = false;
+
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
+$gameTime.addEventListener('input', setGameTime);
+
+function startGame() {
+  score = 0;
+  $timeHeader.classList.remove('hide');
+  $resultHeader.classList.add('hide');
+  setGameTime();
+  $gameTime.setAttribute('disabled', 'true');
+  isGameStarted = true;
+  $game.style.backgroundColor = '#fff';
+  $start.classList.add('hide');
+
+  var interval = setInterval(function () {
+    var time = parseFloat($time.textContent);
+
+    if (time <= 0) {
+      clearInterval(interval);
+      endGame();
+    } else {
+      $time.textContent = (time - 0.1).toFixed(1);
+    }
+  }, 100);
+
+  renderBox();
+}
+
+function endGame() {
+  isGameStarted = false;
+  setGameScore();
+  $gameTime.removeAttribute('disabled');
+  $start.classList.remove('hide');
+  $game.innerHTML = '';
+  $game.style.backgroundColor = '#ccc';
+  $timeHeader.classList.add('hide');
+  $resultHeader.classList.remove('hide');
+}
+
+function setGameScore() {
+  $result.textContent = score.toString();
+}
+
+function setGameTime() {
+  var time = +$gameTime.value;
+  $time.textContent = time.toFixed(1);
+}
+
+function handleBoxClick(event) {
+  if (!isGameStarted) {
+    return;
+  }
+  if (event.target.dataset.box) {
+    score++;
+    renderBox();
+  }
+}
+
+function renderBox() {
+  $game.innerHTML = '';
+  var boxSize = getRandom(30, 100);
+  var box = document.createElement('div');
+  var gameSize = $game.getBoundingClientRect();
+  var maxTop = gameSize.height - boxSize;
+  var maxLeft = gameSize.width - boxSize;
+
+  box.style.height = box.style.width = boxSize + 'px';
+  box.style.position = 'absolute';
+  box.style.backgroundColor = '#' + getRandom(100, 900);
+  box.style.top = getRandom(0, maxTop) + 'px';
+  box.style.left = getRandom(0, maxLeft) + 'px';
+  box.style.cursor = 'pointer';
+  box.setAttribute('data-box', 'true');
+
+  $game.insertAdjacentElement('afterbegin', box);
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
